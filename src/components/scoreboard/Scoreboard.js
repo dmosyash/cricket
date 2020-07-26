d6a427e2565b9ec4788c0fb1662c4cb85a90d80f
Yash Kochar <yashkochar@nexteducation.com>
2020-07-26 16:21:12 +0530

    removing unnecessary ref

--- src/components/scoreboard/Scoreboard.js
+++ src/components/scoreboard/Scoreboard.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import BoardDetails from './../board-details/BoardDetails';
 
 import './scoreboard.css';
-import teams from '../../services/teams';
 
 /**
  * @name Scoreboard
